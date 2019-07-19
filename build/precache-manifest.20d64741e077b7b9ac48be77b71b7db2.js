69c7f9bc6f72b081350b047b9cb3cfb1d8774ae5
Theo Gosselink <t.gosselink@breman.nl>
2019-07-19 10:32:42 +0200

    Build added

---
+++ build/precache-manifest.20d64741e077b7b9ac48be77b71b7db2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910f0b2bd5b7ee06e69d5351b46f73b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ab609c031e988ed5c3e",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "65d156b09348a533fbae",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "3ab609c031e988ed5c3e",
+    "url": "/static/js/2.86fb08d4.chunk.js"
+  },
+  {
+    "revision": "65d156b09348a533fbae",
+    "url": "/static/js/main.7e49f4cf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
