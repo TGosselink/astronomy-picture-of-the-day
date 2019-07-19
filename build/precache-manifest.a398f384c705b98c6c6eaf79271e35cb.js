c273c56727baf2484a7bbf9ce72dfa2bb4849d81
Theo Gosselink <t.gosselink@breman.nl>
2019-07-19 10:34:50 +0200

    Build folder added

---
+++ build/precache-manifest.a398f384c705b98c6c6eaf79271e35cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0bdb3b0bf35e7591c902abb3416684",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "3ab609c031e988ed5c3e",
+    "url": "/build/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "6a5f24b59fd36cdd503c",
+    "url": "/build/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "3ab609c031e988ed5c3e",
+    "url": "/build/static/js/2.86fb08d4.chunk.js"
+  },
+  {
+    "revision": "6a5f24b59fd36cdd503c",
+    "url": "/build/static/js/main.83b99c52.chunk.js"
+  },
+  {
+    "revision": "b4d8ef2cb7791815737b",
+    "url": "/build/static/js/runtime~main.9df7376b.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20d64741e077b7b9ac48be77b71b7db2.js"
+  "/build/precache-manifest.a398f384c705b98c6c6eaf79271e35cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
